67240b497730e0888f598611d9a5fd9763640a84
Edwin Millan <bassgs3000@gmail.com>
2014-07-10 14:34:52 -0500

    Added Basic Models for inventory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :assets
+
+  resources :locations
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

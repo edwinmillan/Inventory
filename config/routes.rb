a92dacd2a0a01b067cab3f1d443625346d7031dd
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2014-08-25 13:49:25 -0500

    Added home location

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+
   resources :assets
 
   resources :locations
 
   resources :users
+
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
